dd8b989181d49f8a98c4a38ad4178e9b1426475b
Prodromosk <makiskahrimanidis@gmail.com>
2019-10-25 22:35:27 +0200

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-import MovieRow from './movies'
+import MovieRow from './components/movies'
 import $ from 'jquery'
 
 class App extends Component {
@@ -47,31 +47,15 @@ class App extends Component {
     <div className="App">
 
 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a className="navbar-brand" href="#">MoPi Search</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul className="navbar-nav">
-      <li className="nav-item active">
-        <a className="nav-link" href={""}>Home <span className="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href={""}>Loved</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href={""}>Watch later</a>
-      </li>
-    </ul>
-  </div>
+  <a className="navbar-brand" href={""}>MoPi Search</a>
 </nav>
 
 <div className="searchBox has-search">
   <span className="fa fa-search form-control-feedback"></span>
   <input onChange= {this.searchMovieInput.bind(this)}
-        className="form-control" type="text"
-        placeholder="Search for a movie"
-        aria-label="Search"></input>
+         className="form-control" type="text"
+         placeholder="Search for a movie"
+         aria-label="Search"></input>
   </div>
       {this.state.rows}
     </div>
