45fa82f38b6f1b9c0405cf8aad06053d9c94694a
prodester <makiskahrimanidis@gmail.com>
2019-10-25 21:09:04 +0200

    bootstrap

--- src/movies.js
+++ src/movies.js
@@ -9,19 +9,18 @@ class MovieRow extends React.Component {
   render() {
 
     return (
-      <table key= {this.props.movie.id}>
-        <tbody>
-          <tr>
-            <td>
-            <img onError= {this.fallBackImg} alt={this.props.movie.title} src={this.props.movie.moviePoster}/>
-            </td>
-            <td>
-            <h1>{this.props.movie.title}</h1>
-            <p>{this.props.movie.overview}</p>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+
+      <div className="media" key= {this.props.movie.id}>
+        <img className= "moviePosterImg" onError= {this.fallBackImg}
+             alt={this.props.movie.title}
+             src={this.props.movie.moviePoster}/>
+
+        <div className= "media-body">
+          <h3 className= "mt-0">{this.props.movie.title}</h3>
+          {this.props.movie.overview}
+        </div>
+        <hr></hr>
+      </div>
     )
   }
 }
