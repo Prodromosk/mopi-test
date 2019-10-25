73f56e0582fc673362f97f530b1f8303ddcf2a81
prodester <makiskahrimanidis@gmail.com>
2019-10-25 10:14:21 +0200

    fe changes

--- src/App.js
+++ src/App.js
@@ -46,20 +46,33 @@ class App extends Component {
     return (
     <div className="App">
 
-      <table className="moviesDbBar">
-        <tbody>
-          <tr>
-            <td>
-              <img alt="The Movies DB Logo" className="moviesDbLogo" src="moviesdb.svg"/>
-            </td>
-            <td>
-              <h3>Mopi Search</h3>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <input onChange= {this.searchMovieInput.bind(this)} placeholder="Search for a movie">
-      </input>
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a className="navbar-brand" href="#">MoPi Search</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link" href={""}>Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href={""}>Loved</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href={""}>Watch later</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div className="searchBox has-search">
+  <span className="fa fa-search form-control-feedback"></span>
+  <input onChange= {this.searchMovieInput.bind(this)}
+        className="form-control" type="text"
+        placeholder="Search for a movie"
+        aria-label="Search"></input>
+  </div>
       {this.state.rows}
     </div>
   );
